1d73061f8a7a124aea415608f656965d563ef3c8
haiqing.xu <haiqing.xu@ly.com>
2026-05-09 20:00:17 +0800

    feat: 1.3.6

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import { getLogger, LogLevel } from './utils/logger.js';
 import chalk from 'chalk';
 import figlet from 'figlet';
 
-const VERSION = '1.3.5';
+const VERSION = '1.3.6';
 const logger = getLogger();
 
 // Handle environment variables
